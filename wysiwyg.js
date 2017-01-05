edb29c8601a2ef33a63b1085128075b07cd3326e
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-05 11:12:13 -0600

    files load on page

--- wysiwyg.js
+++ wysiwyg.js
@@ -21,8 +21,8 @@ function fillPage() {
   var peopleHTML = "";
   for(var i = 0; i < famousPeople.people.length; i++) {
     peopleHTML += `<div class="cards"><h2>Name: ${famousPeople.people[i].name}</h2>`;
-    peopleHTML += `<p class="bio">Bio: ${famousePeople.people[i].bio}</p>`;
-    peopleHTML += `<img src=${famousPeople.people[i].image} alt="image of ${famousPeople.people[i].name}">`;
+    peopleHTML += `<p class="bio">Bio: ${famousPeople.people[i].bio}</p>`;
+    peopleHTML += `<img src=${famousPeople.people[i].image} alt="image of ${famousPeople.people[i].name}" width="200" height="200">`;
   }
   peopleHTML += `</div>`;
   $(".container").html(peopleHTML);
