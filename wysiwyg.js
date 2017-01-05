0d7ccb79dd7770868196d18aafea1bc85b345c4a
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-05 09:05:18 -0600

    base javascript

---
+++ wysiwyg.js
@@ -0,0 +1,34 @@
+//Global Var
+
+var famousPeople;
+
+var promise1 = new Promise(function(resolve, reject) {
+    $.ajax({
+      url : "famousPeople.json"
+    })
+    .done(function(data, textStatus, XHR){
+      resolve(data);
+      console.log(data);
+    })
+  });
+
+promise1.then(function(val){
+  famousPeople = val;
+  fillPage();
+});
+
+function fillPage() {
+  var peopleHTML = "";
+  for(var i = 0; i < famousPeople.people.length; i++) {
+    peopleHTML += `<div class="cards"><h2>Name: ${famousPeople.people[i].name}</h2>`;
+    peopleHTML += `<p class="bio">Bio: ${famousePeople.people[i].bio}</p>`;
+    peopleHTML += `<img src=${famousPeople.people[i].image} alt="image of ${famousPeople.people[i].name}">`;
+  }
+  peopleHTML += `</div>`;
+  $(".container").html(peopleHTML);
+}
+
+
+function colorCards() {
+  $( ".cards:even" ).css( "background-color", "#bbf" );
+};
