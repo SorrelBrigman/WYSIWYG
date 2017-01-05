f480fc6a2a9f74f3c9454a6e497ff3cfff6f4a73
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-05 11:28:25 -0600

    added coloring of cards based on even or odd

--- wysiwyg.js
+++ wysiwyg.js
@@ -20,15 +20,15 @@ promise1.then(function(val){
 function fillPage() {
   var peopleHTML = "";
   for(var i = 0; i < famousPeople.people.length; i++) {
-    peopleHTML += `<div class="cards"><h2>Name: ${famousPeople.people[i].name}</h2>`;
+    if(i%2 === 0) {
+      peopleHTML += `<div class="cards even">`;
+    } else {
+      peopleHTML += `<div class="cards odd">`;
+    }
+    peopleHTML += `<h2>Name: ${famousPeople.people[i].name}</h2>`;
     peopleHTML += `<p class="bio">Bio: ${famousPeople.people[i].bio}</p>`;
     peopleHTML += `<img src=${famousPeople.people[i].image} alt="image of ${famousPeople.people[i].name}" width="200" height="200">`;
   }
   peopleHTML += `</div>`;
   $(".container").html(peopleHTML);
 }
-
-
-function colorCards() {
-  $( ".cards:even" ).css( "background-color", "#bbf" );
-};
