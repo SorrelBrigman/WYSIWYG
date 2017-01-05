fbeef5b189c4ea27684667e59d853ba3bcc4c3fa
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-05 11:42:01 -0600

    dotted border on click now works

--- wysiwyg.js
+++ wysiwyg.js
@@ -25,10 +25,23 @@ function fillPage() {
     } else {
       peopleHTML += `<div class="cards odd">`;
     }
-    peopleHTML += `<h2>Name: ${famousPeople.people[i].name}</h2>`;
+    peopleHTML += `<h2 class="name">Name: ${famousPeople.people[i].name}</h2>`;
     peopleHTML += `<p class="bio">Bio: ${famousPeople.people[i].bio}</p>`;
-    peopleHTML += `<img src=${famousPeople.people[i].image} alt="image of ${famousPeople.people[i].name}" width="200" height="200">`;
+    peopleHTML += `<img class="image" src=${famousPeople.people[i].image} alt="image of ${famousPeople.people[i].name}" width="200" height="200">`;
   }
   peopleHTML += `</div>`;
   $(".container").html(peopleHTML);
+  $(".name").click(dottedBorder);
+  $(".bio").click(dottedBorder);
+  $(".image").click(dottedBorder);
+}
+
+//When you click on a person's element, a dotted border should go around it.
+
+function dottedBorder(evt) {
+  console.log("I've been clicked", evt.target)
+  //if any instances of it occur already, remove it
+  $(".dottedBorderSelector").removeClass("dottedBorderSelector");
+  // add the dotted border to the most recently clicked element
+  $(evt.target).addClass("dottedBorderSelector");
 }
